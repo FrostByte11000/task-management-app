28165cad92f97beea793dc4add2d787f2fcd5c28
Sparsha Yogish <yogish.1@buckeyemail.osu.edu>
2026-09-13 17:01:07 -0400

    Set up Express Backend

---
+++ backend/src/server.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.get("/api/status", (req, res) => {
+  res.json({
+    status: "ok",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
